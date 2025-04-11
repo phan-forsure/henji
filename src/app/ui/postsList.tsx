64de970424ebc475a30baec31d51c7ac458767f4
phan-forsure <phantommy38@gmail.com>
2025-04-11 11:00:00 +0200

    edit

---
+++ src/app/ui/postsList.tsx
@@ -0,0 +1,28 @@
+import { fetchPosts } from "../lib/data";
+import Post from "./post";
+
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
+export default async function PostsList() {
+  const posts = await fetchPosts();
+
+  return (
+    <>
+      {posts.length == 0 && (
+        <h1 className="text-3xl w-full h-full flex text-center items-center justify-center px-16">
+          No posts available at the moment
+        </h1>
+      )}
+      {posts.map((post) => (
+        <Post
+          key={post.id}
+          title={post.post_title}
+          author={post.post_author}
+          text={post.post_text}
+          date={post.created_at.toString().slice(0, 15)}
+        />
+      ))}
+    </>
+  );
+}
